200cc426725995b49f1e489fb2fdb00cc1f212f4
Herikle Mesquita <herikle.mesquita@gmail.com>
2023-05-28 01:13:03 -0300

    use helmet and tsconfig-paths is not a dev dependency

--- app-config/middlewares.ts
+++ app-config/middlewares.ts
@@ -3,13 +3,14 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import winston from "winston";
 import morgan from "morgan";
+import helmet from "helmet";
 
 const setupMiddlewares = (app) => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(cookieParser());
   app.use(cors());
-
+  app.user(helmet);
   // Log requests
   app.use(
     morgan("tiny", {
