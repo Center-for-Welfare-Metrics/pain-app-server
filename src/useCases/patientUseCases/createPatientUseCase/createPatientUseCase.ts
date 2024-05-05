ae84db0bc8919ed2dcfac94821468a4b94bb3b87
Herikle Mesquita <herikle.mesquita@gmail.com>
2024-05-05 01:09:44 -0300

    update subject name when created as guest user

--- src/useCases/patientUseCases/createPatientUseCase/createPatientUseCase.ts
+++ src/useCases/patientUseCases/createPatientUseCase/createPatientUseCase.ts
@@ -5,18 +5,25 @@ import {
 
 type CreatePatientUseCaseParams = {
   user_id: string;
+  createdAsGuest?: boolean;
 };
 
 export const CreatePatientUseCase = async (
   params: CreatePatientUseCaseParams
 ) => {
-  const { user_id } = params;
+  const { user_id, createdAsGuest } = params;
 
   const patientsCount = await CountPatientsImplementation({ user_id });
 
+  let patientName = `Subject ${patientsCount + 1}`;
+
+  if (createdAsGuest) {
+    patientName += " (Created as guest)";
+  }
+
   const newPatient = await CreatePatientImplementation({
     user_id,
-    name: `Subject ${patientsCount + 1}`,
+    name: patientName,
     type: "human",
   });
 
